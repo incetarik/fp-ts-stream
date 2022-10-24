02192843010de816d6b672e9b41475fb7ea9b8ff
Tarık İnce <incetarik@hotmail.com>
2022-10-24 21:37:10 +0300

    Added `Applicative` implementation

---
+++ src/async-stream/applicative.ts
@@ -0,0 +1,54 @@
+import { Applicative1 } from 'fp-ts/lib/Applicative'
+
+import { Functor } from './functor'
+import { Pointed } from './pointed'
+import { AsyncStream, URI } from './uri'
+
+/**
+ * Applies a {@link AsyncStream} of type `A` to {@link AsyncStream} of functions
+ * from `A` to `B`.
+ *
+ * @export
+ * @template A The value type.
+ * @param {AsyncStream<A>} fa The source async stream.
+ * @return {(fab: AsyncStream<(a: A) => B>) => AsyncStream<B>} A function that
+ * takes a {@link AsyncStream} of functions.
+ * 
+ * @__PURE__
+ */
+export function ap<A>(fa: AsyncStream<A>) {
+  /**
+   * Applies previously given {@link AsyncStream} to the
+   * given {@link AsyncStream} of functions that take the items of the
+   * previously given {@link AsyncStream} and returns `B` values.
+   *
+   * @template B The output value type.
+   * @param {AsyncStream<(a: A) => B>} fab The async function stream to
+   * apply the values of the previously given async stream to its functions
+   * returning `B` value.
+   * 
+   * @return {AsyncStream<B>} The output async stream.
+   * @step 1
+   * 
+   * @__PURE__
+   */
+  return function _ap<B>(fab: AsyncStream<(a: A) => B>): AsyncStream<B> {
+    return async function* __ap() {
+      for await (const a of fa()) {
+        for await (const f of fab()) {
+          yield f(a)
+        }
+      }
+    }
+  }
+}
+
+/**
+ * The `Applicative` category instance for {@link AsyncStream}.
+ */
+export const Applicative: Applicative1<URI> = {
+  URI,
+  map: Functor.map,
+  of: Pointed.of,
+  ap(fab, fa) { return ap(fa)(fab) }
+}
