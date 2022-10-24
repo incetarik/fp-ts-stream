8626badd73f35754926e91a6ec5decff65b45d83
Tarık İnce <incetarik@hotmail.com>
2022-10-24 21:32:40 +0300

    Added `AsyncStream URI` interface

---
+++ src/async-stream/uri.ts
@@ -0,0 +1,31 @@
+
+/**
+ * Describes a function which returns a {@link AsyncGenerator} of `A` values.
+ *
+ * @export
+ * @interface AsyncStream
+ * @template A The value type.
+ */
+export interface AsyncStream<A> {
+  (): AsyncGenerator<A>
+}
+
+/**
+ * The type URI of the {@link AsyncStream} instances.
+ * 
+ * @category type lambdas
+ */
+export const URI = 'AsyncStream'
+
+/**
+ * The type URI of the {@link AsyncStream} instances.
+ * 
+ * @category type lambdas
+ */
+export type URI = typeof URI
+
+declare module 'fp-ts/HKT' {
+  interface URItoKind<A> {
+    readonly [ URI ]: AsyncStream<A>
+  }
+}
