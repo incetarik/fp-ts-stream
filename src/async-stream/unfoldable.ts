3a2997204d223a4e8129228d4c877d6d3297e4cb
Tarık İnce <incetarik@hotmail.com>
2022-10-24 21:48:39 +0300

    Added `Unfoldable` for implementation

---
+++ src/async-stream/unfoldable.ts
@@ -0,0 +1,38 @@
+import { isSome, Option } from 'fp-ts/lib/Option'
+import { Unfoldable1 } from 'fp-ts/lib/Unfoldable'
+
+import { AsyncStream, URI } from './uri'
+
+/**
+ * Takes afunction which returns an {@link Option} of a tuple containing an
+ * outcome value and an input for the following iteration.
+ * 
+ * This function applies given `f` to the inital value `b` and then recursively
+ * to the second element of the tuple contained in the returned {@link Option}
+ * of the previous calculation until `f` returns `None`.
+ *
+ * @export
+ * @template A The value type.
+ * @template B The initial _seed_ value type.
+ * @param {B} b The initial value.
+ * @param {(b: B) => Option<readonly [ A, B ]>} f A function that recursively
+ * provides value-seed tuple for the output async stream.
+ * 
+ * @returns {AsyncStream<A>} The output async stream.
+ * @__PURE__
+ */
+export function unfold<A, B>(b: B, f: (b: B) => Option<readonly [ A, B ]>): AsyncStream<A> {
+  return async function* _unfold() {
+    for (let current = f(b); isSome(current); current = f(current.value[ 1 ])) {
+      yield current.value[ 0 ]
+    }
+  }
+}
+
+/**
+ * The `Unfoldable` category instance for {@link AsyncStream}.
+ */
+export const Unfoldable: Unfoldable1<URI> = {
+  URI,
+  unfold,
+}
