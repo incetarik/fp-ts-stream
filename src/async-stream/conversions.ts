949a5e10ac0ae45a82bf4c380824ad5aa2e6699a
Tarık İnce <incetarik@hotmail.com>
2022-10-25 17:40:57 +0300

    Added `fromPromises{Seq}` implementations

--- src/async-stream/conversions.ts
+++ src/async-stream/conversions.ts
@@ -60,6 +60,74 @@ export function fromAsyncIterable<A>(a: AsyncIterable<A>): AsyncStream<A> {
 }
 
 /**
+ * Creates an {@link AsyncStream} from an iterable of {@link Promise}s.
+ * 
+ * Yields elements at when a promise in the iterable is resolved. Therefore,
+ * the elements might not be in-order.
+ * 
+ * If the order is important, use {@link fromPromisesSeq}.
+ *
+ * @export
+ * @template A The value type.
+ * @param {Iterable<Promise<A>>} input The input promises.
+ * @return {AsyncStream<A>} An async stream output.
+ * 
+ * @__PURE__
+ */
+export function fromPromises<A>(input: Iterable<Promise<A>>): AsyncStream<A> {
+  return async function* _fromPromises() {
+    const indexMap = new WeakMap<Promise<A>, number>()
+    let size = 0
+    let alreadyCompleted = 0
+
+    const remaining: Promise<[ number, A ]>[] = []
+    const targetIndex: number[] = []
+
+    for (const fa of input) {
+      const oldIndex = size
+      indexMap.set(fa, size - alreadyCompleted)
+      targetIndex.push(size - alreadyCompleted)
+      size++
+
+      remaining.push(fa.then(a => {
+        ++alreadyCompleted
+        const newIndex = targetIndex[ oldIndex ]
+
+        for (let i = oldIndex + 1; i < size; ++i) {
+          --targetIndex[ i ]
+        }
+
+        return [ newIndex, a ]
+      }))
+    }
+
+    while (remaining.length) {
+      const [ newIndex, a ] = await Promise.any(remaining)
+      remaining.splice(newIndex, 1)
+      yield a
+    }
+  }
+}
+
+/**
+ * Similar to {@link fromPromises} but the order is preserved.
+ *
+ * @export
+ * @template A The value type.
+ * @param {Iterable<Promise<A>>} input The input promises.
+ * @return {AsyncStream<A>} The async stream output.
+ * 
+ * @__PURE__
+ */
+export function fromPromisesSeq<A>(input: Iterable<Promise<A>>): AsyncStream<A> {
+  return async function* _fromPromisesSeq() {
+    for (const fa of input) {
+      yield await fa
+    }
+  }
+}
+
+/**
  * Converts an {@link AsyncStream} to a {@link Task} of the array of
  * the elements.
  *
