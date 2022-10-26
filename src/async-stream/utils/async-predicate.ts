0395aab7f77903f339b4217c4ac6a4da9f6f903a
Tarık İnce <incetarik@hotmail.com>
2022-10-26 17:05:31 +0300

    Added `AsyncPredicateWithIndex`

--- src/async-stream/utils/async-predicate.ts
+++ src/async-stream/utils/async-predicate.ts
@@ -8,3 +8,14 @@
 export interface AsyncPredicate<A> {
   (a: A): Promise<boolean> | boolean
 }
+
+/**
+ * An interface that represents a function that returns a task of a boolean.
+ *
+ * @export
+ * @interface AsyncPredicateWithIndex
+ * @template A The value type.
+ */
+export interface AsyncPredicateWithIndex<I, A> {
+  (i: I, a: A): Promise<boolean> | boolean
+}
