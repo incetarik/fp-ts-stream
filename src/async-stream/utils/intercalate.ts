bc3e5294f0df933f0da05ca5e26419ce9732ed23
Tarık İnce <incetarik@hotmail.com>
2022-10-26 16:18:40 +0300

    Added `intercalate` for `AsyncStream`

---
+++ src/async-stream/utils/intercalate.ts
@@ -0,0 +1,55 @@
+import { Monoid } from 'fp-ts/lib/Monoid'
+import { Task } from 'fp-ts/lib/Task'
+
+import { AsyncStream } from '../uri'
+import { intersperse } from './intersperse'
+
+/**
+ * Places an element between members of an {@link AsyncStream}.
+ *
+ * @export
+ * @template A The value type.
+ * @param {Monoid<A>} M The monoid instance.
+ * @return {(middle: A) => (fa: AsyncStream<A>) => Task<A>} A function that
+ * takes the element to place between members of the stream.
+ * 
+ * @__PURE__
+ */
+export function intercalate<A>(M: Monoid<A>) {
+  /**
+   * Takes a value to place between members of an {@link AsyncStream}.
+   *
+   * @param {A} middle The value to place.
+   * @return {(fa: AsyncStream<A>) => A} A function that takes an async stream
+   * and returns the value.
+   * 
+   * @step 1
+   * @__PURE__
+   */
+  return function _intercalate(middle: A) {
+    /**
+     * Takes a stream and places the previously given element between the
+     * members of the given stream.
+     *
+     * @param {Stream<A>} fa The input stream.
+     * @return {A} The concatenated value with the previously given monoid
+     * instance.
+     * 
+     * @step 2
+     * @__PURE__
+     */
+    return function __intercalate(fa: AsyncStream<A>): Task<A> {
+      return async function ___intercalate() {
+        let curr = M.empty
+        const interspersed = intersperse(middle)(fa)
+
+        for await (const a of interspersed()) {
+          curr = M.concat(curr, a)
+        }
+
+        return curr
+      }
+    }
+  }
+}
+
